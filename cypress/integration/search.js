236d06bd3b1619b8e0d0a2bf79727db7ede062d0
alberto.perez <alberto.perez@revolt.digital>
2022-05-09 10:43:48 -0300

    Using custom commands to the tests

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -7,11 +7,7 @@ describe('Search Elements', ()=>{
 
     it('Search for elements with multiple results', ()=>{
         
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('dress');
-            cy.get(index.searchButton).click();
-        })
-
+        cy.search('dress');
         cy.fixture('searchResult').then((searchResult)=>{
             cy.get(searchResult.title).should('contain','dress');
         })
@@ -19,14 +15,10 @@ describe('Search Elements', ()=>{
 
     it('Search for elements with no reults', ()=>{
         
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('querty');
-            cy.get(index.searchButton).click();
-        })
-
-        cy.fixture('searchResult').then((searchResult)=>{
-            cy.get(searchResult.alert).should('contain','No results were found for your search');
-        })
+       cy.search('query');
+       cy.fixture('searchResult').then((searchResult)=>{
+           cy.get(searchResult.alert).should('contain','No results were found for your search');
+       })
     })
 
 })
